93b1141973ec4d799a69e658a067922498ee4588
caasi Huang <caasi.igd@gmail.com>
2013-09-21 01:53:18 +0800

    first working example stub

--- examples/demo/source/main.cpp
+++ examples/demo/source/main.cpp
@@ -8,19 +8,68 @@ using namespace video;
 using namespace io;
 using namespace gui;
 
+class KeyBuffer : public IEventReceiver
+{
+public:
+    KeyBuffer()
+    {
+        for (u32 i = 0; i < KEY_KEY_CODES_COUNT; ++i)
+            status[i] = false;
+    }
+
+    virtual bool OnEvent(const SEvent &event)
+    {
+        if (event.EventType == irr::EET_KEY_INPUT_EVENT)
+            status[event.KeyInput.Key] = event.KeyInput.PressedDown;
+
+        return false;
+    }
+
+    virtual bool isDown(EKEY_CODE keyCode) const
+    {
+        return status[keyCode];
+    }
+
+private:
+    bool status[KEY_KEY_CODES_COUNT];
+};
+
 int main(int argc, char *argv[])
 {
+    KeyBuffer keyBuffer;
+
     IrrlichtDevice *device = createDevice(
-        video::EDT_SOFTWARE,
+        video::EDT_OPENGL,
         dimension2d<u32>(640, 480),
         16,
         false,
         false,
         false,
-        0
+        &keyBuffer
     );
 
     if (!device) return 1;
+
+    device->setWindowCaption(L"demo");
+
+    IVideoDriver *driver = device->getVideoDriver();
+    ISceneManager *smgr = device->getSceneManager();
+    IGUIEnvironment *guienv = device->getGUIEnvironment();
+
+    smgr->addCubeSceneNode();
+    smgr->addCameraSceneNode(0, vector3df(0, 30, -40), vector3df(0, 5, 0));
+
+    while (device->run())
+    {
+        if (keyBuffer.isDown(KEY_ESCAPE)) device->closeDevice();
+
+        driver->beginScene(true, true, SColor(255, 22, 22, 29));
+
+        smgr->drawAll();
+        guienv->drawAll();
+
+        driver->endScene();
+    }
 
     device->drop();
 
